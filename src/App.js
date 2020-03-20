434ed9e4cffe8835ea5177db7e8594ea63b72399
Kevin Montero <kevinwilding1997@gmail.com>
2020-03-19 20:55:26 -0400

    quote component done and starting Developing component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import './App.sass';
-import Nav from'./Components/Nav/Nav'
-import Features from './Components/Features/Features'
-import NewLiving from './Components/NewLiving/NewLiving'
+import Nav from './Components/Nav/Nav';
+import Features from './Components/Features/Features';
+import NewLiving from './Components/NewLiving/NewLiving';
+import Quote from './Components/Quote/Quote';
 
 function App() {
-  return (
-    <div className="App">
-      <Nav />
-      <Features/>
-      <NewLiving/>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Nav />
+			<div className='App-container'>
+				<Features />
+				<NewLiving />
+				<Quote />
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/Components/Quote/Quote.js
+++ src/Components/Quote/Quote.js
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import './Quote.sass'
+import background  from '../../img/gallery-01.jpg'
 const Quote = () => {
     return (
-        <div>
-            
+        <div className='Quote'>
+            <img src={background} alt="house background"/>
+            <i className="fas fa-quote-left"></i>
+            <h1>Cras nisl lacus, tincidunt vitae neque vitae, interdum iaculis tortor. Orci varius natoque penatibus et magnis dis parturient montes</h1>
+            <p>John Doe</p>
+
         </div>
     )
 }
