4735dec6885813a4cac28fd9af93675cee21f573
Kevin Montero <kevinwilding1997@gmail.com>
2020-03-22 23:46:31 -0400

    quote component done

---
+++ src/Components/Developing/Developing.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Developing = () => {
+    return (
+        <div className='Developing'>
+            <h1>Developing Process</h1>
+            <div className="content">
+                <div className="left-side">
+                    <img src="" alt=""/>
+                </div>
+                <div className="right-side">
+
+                </div>
+            </div>
+        </div>  
+    )
+}
+
+export default Developing
