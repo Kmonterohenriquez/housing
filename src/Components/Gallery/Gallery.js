4bcc42b7b215117f7020f5ba0c306cb9a8d16060
Kevin Montero <kevinwilding1997@gmail.com>
2020-03-27 12:42:35 -0400

    Starting on Gallery component

---
+++ src/Components/Gallery/Gallery.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Gallery.sass'
+const Gallery = () => {
+    return (
+        <div className='Gallery'>
+            <h1 className="Gallery-title">Gallery</h1>
+            
+        </div>
+    )
+}
+
+export default Gallery
