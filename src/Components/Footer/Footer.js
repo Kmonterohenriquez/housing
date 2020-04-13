2e068d1694e2b48a2a1f308838d1d834294f6a21
Kevin Montero <kevinwilding1997@gmail.com>
2020-04-13 00:12:20 -0400

    logo added onto Footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import './Footer.sass'
+import white_logo from '../../img/white_logo.png'
 const Footer = () => {
     return (
         <div className='Footer'>
-            
+            <img src={white_logo} alt="Oberlo white logo"/>
         </div>
     )
 }
